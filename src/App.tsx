97eb34cc14317e908ef901ddb37e9c801cdeb743
Ксюша Кисель <kkseniya2004@yahoo.com>
2025-07-08 12:35:06 +0300

    add hw 42

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,18 @@ import { SignInForm } from './Components/Form/SignInForm';
 import { Success } from './Components/Confirmation/Success';
 import PostCardList from './Components/Posts/PostCardList';
 import SearchPage from './Components/Pages/SearchPage';
+import { ThemeProvider } from './Components/Theme/ThemeContext';
 
 function App() {
 
   const [state, setState] = useState(false);
 
   return (
-    <><Layout title={state ? 'Success' : 'Sign In'}>
-      {state ? <Success onClick={() => setState(false)} /> : <SignInForm onClick={() => setState(true)} />}
-    </Layout><PostCardList /><SearchPage /></>
-
+    <ThemeProvider>
+      <><Layout title={state ? 'Success' : 'Sign In'}>
+        {state ? <Success onClick={() => setState(false)} /> : <SignInForm onClick={() => setState(true)} />}
+      </Layout><PostCardList /><SearchPage /></>
+    </ThemeProvider>
   )
 }
 
