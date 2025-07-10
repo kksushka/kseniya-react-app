acaa381f284b0797d9246e8650254895a02c8ae9
Ксюша Кисель <kkseniya2004@yahoo.com>
2025-07-10 13:40:57 +0300

    add changes

--- src/Components/Forms/Success.tsx
+++ src/Components/Forms/Success.tsx
@@ -6,7 +6,7 @@ export function Success({ onClick }: SuccessProps) {
     return (
         <div className="success">
             <p className="success__text"> Nice to see you again!</p>
-            <button onClick={onClick} className="success__btn">Go to home</button>
+            <button onClick={onClick} className="success__btn">Go home</button>
         </div>
     )
 }
